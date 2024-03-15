470083fffda8ecfa9fbb349b31ca56e867ef4db8
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-03-14 23:14:52 -0300

    feat: create project base & first components

---
+++ components/Presente.tsx
@@ -0,0 +1,14 @@
+import styles from '../src/styles/Presente.module.css';
+
+const Presente = () => {
+  return (
+    <div className={styles.presente}>
+      <div className={styles.tampa}></div>
+      <div className={styles.corpo}></div>
+      <div className={styles.laco1}></div>
+      <div className={styles.laco2}></div>
+    </div>
+  );
+}
+
+export default Presente;
